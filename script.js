ce5718a650a0266a0bbd74148f565f9bd1e84f36
Muhammad Fuzail Raza <rmuhammadfuzail@gmail.com>
2026-05-12 03:34:21 -0700

    css or javaScript ki files alag ki hn

---
+++ script.js
@@ -0,0 +1,546 @@
+let products = [
+  {
+    id: 1,
+    name: "Apple",
+    price: 120,
+    category: "Fruits",
+    image: "./images/apple.jpg",
+  },
+
+  {
+    id: 2,
+    name: "Banana",
+    price: 80,
+    category: "Fruits",
+    image: "./images/banana.jpg",
+  },
+
+  {
+    id: 3,
+    name: "Orange",
+    price: 140,
+    category: "Fruits",
+    image: "./images/orange.jpg",
+  },
+
+  {
+    id: 4,
+    name: "Mango",
+    price: 200,
+    category: "Fruits",
+    image: "./images/mango.jpg",
+  },
+
+  {
+    id: 5,
+    name: "Grapes",
+    price: 170,
+    category: "Fruits",
+    image: "./images/grapes.jpg",
+  },
+
+  {
+    id: 6,
+    name: "Strawberry",
+    price: 250,
+    category: "Fruits",
+    image: "./images/strawberry.jpg",
+  },
+
+  {
+    id: 7,
+    name: "Watermelon",
+    price: 300,
+    category: "Fruits",
+    image: "./images/watermelon.jpg",
+  },
+
+  {
+    id: 8,
+    name: "Pineapple",
+    price: 220,
+    category: "Fruits",
+    image: "./images/pineapple.jpg",
+  },
+
+  {
+    id: 9,
+    name: "Potato",
+    price: 90,
+    category: "Vegetables",
+    image: "./images/potato.jpg",
+  },
+
+  {
+    id: 10,
+    name: "Tomato",
+    price: 110,
+    category: "Vegetables",
+    image: "./images/tomato.jpg",
+  },
+
+  {
+    id: 11,
+    name: "Onion",
+    price: 130,
+    category: "Vegetables",
+    image: "./images/onion.jpg",
+  },
+
+  {
+    id: 12,
+    name: "Carrot",
+    price: 100,
+    category: "Vegetables",
+    image: "./images/carrot.jpg",
+  },
+
+  {
+    id: 13,
+    name: "Cucumber",
+    price: 95,
+    category: "Vegetables",
+    image: "./images/cucumber.jpg",
+  },
+
+  {
+    id: 14,
+    name: "Capsicum",
+    price: 180,
+    category: "Vegetables",
+    image: "./images/capsicum.jpg",
+  },
+
+  {
+    id: 15,
+    name: "Broccoli",
+    price: 260,
+    category: "Vegetables",
+    image: "./images/broccoli.jpg",
+  },
+
+  {
+    id: 16,
+    name: "Spinach",
+    price: 70,
+    category: "Vegetables",
+    image: "./images/spinach.jpg",
+  },
+
+  {
+    id: 17,
+    name: "Milk",
+    price: 250,
+    category: "Dairy",
+    image: "./images/milk.jpg",
+  },
+
+  {
+    id: 18,
+    name: "Cheese",
+    price: 450,
+    category: "Dairy",
+    image: "./images/cheese.jpg",
+  },
+
+  {
+    id: 19,
+    name: "Butter",
+    price: 350,
+    category: "Dairy",
+    image: "./images/butter.jpg",
+  },
+
+  {
+    id: 20,
+    name: "Yogurt",
+    price: 180,
+    category: "Dairy",
+    image: "./images/yogurt.jpg",
+  },
+
+  {
+    id: 21,
+    name: "Cream",
+    price: 220,
+    category: "Dairy",
+    image: "./images/cream.jpg",
+  },
+
+  {
+    id: 22,
+    name: "Bread",
+    price: 150,
+    category: "Bakery",
+    image: "./images/bread.jpg",
+  },
+
+  {
+    id: 23,
+    name: "Cake",
+    price: 600,
+    category: "Bakery",
+    image: "./images/cake.jpg",
+  },
+
+  {
+    id: 24,
+    name: "Donut",
+    price: 120,
+    category: "Bakery",
+    image: "./images/donut.jpg",
+  },
+
+  {
+    id: 25,
+    name: "Cookies",
+    price: 200,
+    category: "Bakery",
+    image: "./images/cookies.jpg",
+  },
+
+  {
+    id: 26,
+    name: "Croissant",
+    price: 280,
+    category: "Bakery",
+    image: "./images/croissant.jpg",
+  },
+
+  {
+    id: 27,
+    name: "Chicken",
+    price: 750,
+    category: "Meat",
+    image: "./images/chicken.jpg",
+  },
+
+  {
+    id: 28,
+    name: "Beef",
+    price: 1200,
+    category: "Meat",
+    image: "./images/beef.jpg",
+  },
+
+  {
+    id: 29,
+    name: "Fish",
+    price: 900,
+    category: "Meat",
+    image: "./images/fish.jpg",
+  },
+
+  {
+    id: 30,
+    name: "Eggs",
+    price: 300,
+    category: "Meat",
+    image: "./images/egg.jpg",
+  },
+
+  {
+    id: 31,
+    name: "Rice",
+    price: 280,
+    category: "Groceries",
+    image: "./images/rice.jpg",
+  },
+
+  {
+    id: 32,
+    name: "Sugar",
+    price: 170,
+    category: "Groceries",
+    image: "./images/sugar.jpg",
+  },
+
+  {
+    id: 33,
+    name: "Salt",
+    price: 60,
+    category: "Groceries",
+    image: "./images/salt.jpg",
+  },
+
+  {
+    id: 34,
+    name: "Tea",
+    price: 500,
+    category: "Groceries",
+    image: "./images/tea.jpg",
+  },
+
+  {
+    id: 35,
+    name: "Coffee",
+    price: 750,
+    category: "Groceries",
+    image: "./images/coffee.jpg",
+  },
+
+  {
+    id: 36,
+    name: "Oil",
+    price: 650,
+    category: "Groceries",
+    image: "./images/oil.jpg",
+  },
+
+  {
+    id: 37,
+    name: "Juice",
+    price: 220,
+    category: "Drinks",
+    image: "./images/juice.jpg",
+  },
+
+  {
+    id: 38,
+    name: "Cold Drink",
+    price: 120,
+    category: "Drinks",
+    image: "./images/cola.jpg",
+  },
+
+  {
+    id: 39,
+    name: "Mineral Water",
+    price: 80,
+    category: "Drinks",
+    image: "./images/water.jpg",
+  },
+
+  {
+    id: 40,
+    name: "Energy Drink",
+    price: 300,
+    category: "Drinks",
+    image: "./images/energy.jpg",
+  },
+
+  {
+    id: 41,
+    name: "Ice Cream",
+    price: 450,
+    category: "Frozen",
+    image: "./images/icecream.jpg",
+  },
+
+  {
+    id: 42,
+    name: "Frozen Pizza",
+    price: 850,
+    category: "Frozen",
+    image: "./images/pizza.jpg",
+  },
+
+  {
+    id: 43,
+    name: "French Fries",
+    price: 350,
+    category: "Frozen",
+    image: "./images/fries.jpg",
+  },
+
+  {
+    id: 44,
+    name: "Burger Patty",
+    price: 550,
+    category: "Frozen",
+    image: "./images/patty.jpg",
+  },
+
+  {
+    id: 45,
+    name: "Soap",
+    price: 180,
+    category: "Personal Care",
+    image: "./images/soap.jpg",
+  },
+
+  {
+    id: 46,
+    name: "Shampoo",
+    price: 420,
+    category: "Personal Care",
+    image: "./images/shampoo.jpg",
+  },
+
+  {
+    id: 47,
+    name: "Toothpaste",
+    price: 240,
+    category: "Personal Care",
+    image: "./images/toothpaste.jpg",
+  },
+
+  {
+    id: 48,
+    name: "Face Wash",
+    price: 380,
+    category: "Personal Care",
+    image: "./images/facewash.jpg",
+  },
+
+  {
+    id: 49,
+    name: "Detergent",
+    price: 500,
+    category: "Cleaning",
+    image: "./images/detergent.jpg",
+  },
+
+  {
+    id: 50,
+    name: "Dish Wash",
+    price: 190,
+    category: "Cleaning",
+    image: "./images/dishwash.jpg",
+  },
+];
+
+let cart = [];
+
+function login() {
+  let name = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+  let password = document.getElementById("password").value;
+
+  if (name === "" || email === "" || password === "") {
+    alert("Please fill all fields");
+    return;
+  }
+
+  if (!email.endsWith("@gmail.com")) {
+    alert("Email must contain @gmail.com");
+    return;
+  }
+
+  if (password.length < 6) {
+    alert("Password must be at least 6 characters");
+    return;
+  }
+
+  document.getElementById("loginPage").style.display = "none";
+
+  document.getElementById("dashboard").style.display = "block";
+
+  document.getElementById("userName").innerText = name;
+
+  document.getElementById("userEmail").innerText = email;
+
+  displayProducts(products);
+
+  displayCategories();
+}
+
+function displayProducts(items) {
+  let productsDiv = document.getElementById("products");
+
+  productsDiv.innerHTML = "";
+
+  items.forEach(function (product) {
+    productsDiv.innerHTML += `
+    
+    <div class="card">
+
+      <img src="${product.image}" alt="">
+
+      <h3>${product.name}</h3>
+
+      <p>Rs ${product.price}</p>
+
+      <button onclick="addToCart(${product.id})">
+        Add To Cart
+      </button>
+
+    </div>
+    
+    `;
+  });
+}
+
+function displayCategories() {
+  let categoryDiv = document.getElementById("categoryList");
+
+  let categories = [];
+
+  products.forEach(function (product) {
+    if (!categories.includes(product.category)) {
+      categories.push(product.category);
+    }
+  });
+
+  categoryDiv.innerHTML = `<button onclick="displayProducts(products)">All</button>`;
+
+  categories.forEach(function (category) {
+    categoryDiv.innerHTML += `
+    
+    <button onclick="filterCategory('${category}')">
+      ${category}
+    </button>
+    
+    `;
+  });
+}
+
+function filterCategory(category) {
+  let filteredProducts = products.filter(function (product) {
+    return product.category === category;
+  });
+
+  displayProducts(filteredProducts);
+}
+
+function searchProduct() {
+  let value = document.getElementById("search").value.toLowerCase();
+
+  let filtered = products.filter(function (product) {
+    return product.name.toLowerCase().includes(value);
+  });
+
+  displayProducts(filtered);
+}
+
+function addToCart(id) {
+  let product = products.find(function (item) {
+    return item.id === id;
+  });
+
+  cart.push(product);
+
+  updateCart();
+}
+
+function updateCart() {
+  let cartItems = document.getElementById("cartItems");
+
+  cartItems.innerHTML = "";
+
+  let total = 0;
+
+  cart.forEach(function (item) {
+    total = total + item.price;
+
+    cartItems.innerHTML += `
+    
+    <div class="cart-item">
+
+      <h4>${item.name}</h4>
+
+      <p>Price: Rs ${item.price}</p>
+
+    </div>
+    
+    `;
+  });
+
+  document.getElementById("totalPrice").innerText = total;
+
+  document.getElementById("cartCount").innerText = cart.length;
+}
+
+function toggleCart() {
+  document.getElementById("cartBox").classList.toggle("active");
+}
